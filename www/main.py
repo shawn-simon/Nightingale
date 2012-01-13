cedcf063df2390e46c4c5d2db93e25ba5a712a83
kdeloach <kdeloach@gmail.com>
2012-01-12 19:03:24 -0500

    added basic login

--- www/main.py
+++ www/main.py
@@ -1,21 +1,23 @@
-import lib.handlers
-import lib.test.testsample
 import os
+import unittest
 import tornado.ioloop
 import tornado.web
-import unittest
+import nightingale.handlers
+import nightingale.tests
+import nightingale.uimodules
 
 settings = {
-    'static_path':   os.path.join(os.path.dirname(__file__), 'static'),
+    'static_path': os.path.join(os.path.dirname(__file__), 'static'),
     'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
-    'login_url':     r"/login",
+    'ui_modules': nightingale.uimodules,
+    'login_url': r"/login",
     'cookie_secret': '3dj1wnExSV6zvypr9y6KFbdDfxwkL0g7ky8VLUWJP3s=',
-    'debug':         True
+    'debug': True
 }
-application = tornado.web.Application(lib.handlers.get_routes(), **settings)
+application = tornado.web.Application(nightingale.handlers.get_routes(), **settings)
 
 if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.loadTestsFromModule(lib.test.testsample)
+    suite = unittest.defaultTestLoader.loadTestsFromModule(nightingale.tests)
     unittest.TextTestRunner().run(suite)
     application.listen(80)
     tornado.ioloop.IOLoop.instance().start()
--- www/schema.sql
+++ www/schema.sql
@@ -1,5 +1,16 @@
-create table abc (
-    id int,
-    name varchar(40)
+create table user (
+    id integer primary key,
+    name varchar(40),
+    hash varchar(60),
+    created datetime,
+    lastlogin datetime
 );
-insert into abc (id, name) values(1, 'test');
+-- 
+create table usercookie (
+    userid int,
+    cookie varchar(40),
+    expires datetime
+);
+
+insert into user (name, hash, created)
+select 'kdeloach', '$2a$12$gTQ9b5wt5GJMvhsk2JebFOFSRJC1ydNKPr56CJD3bkdtIUwEwSkMy', datetime();
