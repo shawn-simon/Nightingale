a39b01caa6c8e021825fcc41509e9558c8d3f523
kdeloach <kdeloach@gmail.com>
2012-02-22 21:23:42 -0500

    added some console output for bootstrap

--- www/nightingale/bootstrap.py
+++ www/nightingale/bootstrap.py
@@ -9,8 +9,12 @@ from nightingale.models import User
 from nightingale.simulation import registerusers
         
 def create_users():
+    print "adding user 1/35..."
     User.addUser(name='kdeloach', password='qwerasdf', namecss='f2 c3')
-    registerusers(30)
+    for n in range(34):
+        print "adding user %d/35..." % (n+1)
+        registerusers(1)
+    print "done"
     
 if __name__ == '__main__':
     drop_database()
